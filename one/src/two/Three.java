d1822cea0864befc9f27f9ecf6732520cb216a48
Yeddanapudi <yeddanapudi1979@yahoo.com>
2023-05-09 11:31:05 +0530

    hi

---
+++ one/src/two/Three.java
@@ -0,0 +1,14 @@
+package two;
+
+public class Three {
+public void test()
+{
+	System.out.println("welcome");
+}
+public static void main(String[] args)
+{
+	Three a=new Three();
+	a.test();
+	
+}
+}
